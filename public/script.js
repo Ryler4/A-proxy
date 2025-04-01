d65f4a00295d1f50fb6bd20af2a2fa8d6d8ee539
Ryler4 <135744675+Ryler4@users.noreply.github.com>
2025-04-01 11:13:17 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,11 @@
 document.getElementById('proxyForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const url = document.getElementById('url').value;
-    const iframe = document.getElementById('contentFrame');
+    const iframe = document.getElementById ('contentFrame');
     iframe.src = `/proxy?url=${encodeURIComponent(url)}`;
+});
+
+document.getElementById('theme').addEventListener('change', function(event) {
+    const selectedTheme = event.target.value;
+    document.body.className = selectedTheme;
 });
