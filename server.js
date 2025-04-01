24fd07505479d19fef653c6ee4f5a27ce071c428
Ryler4 <135744675+Ryler4@users.noreply.github.com>
2025-04-01 11:15:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,23 +9,24 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 // Proxy middleware
-app.use('/proxy', createProxyMiddleware({
-  changeOrigin: true,
-  pathRewrite: (path, req) => {
-    const targetUrl = req.query.url;
-    if (!targetUrl) {
-      return res.status(400).send('No target URL provided');
-    }
-    return targetUrl;
-  },
-  onProxyReq: (proxyReq, req, res) => {
-    console.log(`Proxying request to: ${req.query.url}`);
-  },
-  onError: (err, req, res) => {
-    console.error(err);
-    res.status(500).send('Proxy error');
+app.use('/proxy', (req, res, next) => {
+  const targetUrl = req.query.url;
+  if (!targetUrl) {
+    return res.status(400).send('No target URL provided');
   }
-}));
+
+  createProxyMiddleware({
+    target: targetUrl,
+    changeOrigin: true,
+    onProxyReq: (proxyReq, req, res) => {
+      console.log(`Proxying request to: ${targetUrl}`);
+    },
+    onError: (err, req, res) => {
+      console.error(err);
+      res.status(500).send('Proxy error');
+    }
+  })(req, res, next);
+});
 
 app.listen(PORT, () => {
   console.log(`[+] Proxy server running on port ${PORT}`);
